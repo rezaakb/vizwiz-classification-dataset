5674341bec95dbb077d7e87321bae0dbea2bf4d3
Reza Akbarian Bafghi <r.akbarian98@yahoo.com>
2023-01-11 03:50:59 -0700

    Add files via upload

---
+++ download_dataset.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p dataset/images predictions
+
+wget https://vizwiz.cs.colorado.edu/VizWiz_final/images/train.zip --no-check-certificate
+wget https://vizwiz.cs.colorado.edu/VizWiz_final/images/val.zip --no-check-certificate
+wget https://vizwiz.cs.colorado.edu/VizWiz_final/images/test.zip --no-check-certificate
+
+unzip -q -o train.zip -d dataset/images
+unzip -q -o val.zip -d dataset/images
+unzip -q -o test.zip -d dataset/images
+
+rm train.zip val.zip test.zip
+
+mv -v dataset/images/train/* dataset/images/
+mv -v dataset/images/val/* dataset/images/
+mv -v dataset/images/test/* dataset/images/
